772cb6b38afbdc5c4c04a38180edbb96a66073ce
KolibSoft <kolibsoftware@gmail.com>
2023-08-14 20:00:40 -0600

    MsgHub statics added

--- docs/lib/main.js
+++ docs/lib/main.js
@@ -3,9 +3,11 @@ import { MessageFilters } from "./message_filters.js"
 import { MessageModel } from "./message_model.js"
 import { MessageService } from "./message_service.js"
 import { AuthService } from "./modules/authstore.js"
+import { MsgHubStatics } from "./msghub_statics.js"
 
 export {
     AuthService,
+    MsgHubStatics,
     MessageModel,
     MessageFilters,
     MessageDatabaseCatalogue,
---
+++ docs/lib/msghub_statics.js
@@ -0,0 +1,9 @@
+class MsgHubStatics {
+    get Ready() { return "READY"; }
+    get Sending() { return "SENDING"; }
+    get Delivered() { return "DELIVERED"; }
+}
+
+export {
+    MsgHubStatics
+}
